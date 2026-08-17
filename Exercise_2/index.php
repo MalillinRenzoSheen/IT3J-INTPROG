0f77972c3db3d07db132a74d3cba1df51baba5da
Renzo <malillinrenzosheen_bsit@plmun.edu.ph>
2026-08-18 01:06:33 +0800

    Update 3

--- Exercise_2/index.php
+++ Exercise_2/index.php
@@ -1,5 +1,98 @@
+<?php
+$groupName = "GROUP-4";
+
+$teamMembers = [
+    [
+        "id" => "bryan-camayang",
+        "name" => "Bryan Camayang",
+        "image" => "images/bryan.jpg",
+        "age" => "21 Years Old",
+        "birthdate" => "August 02, 2005",
+        "address" => "Muntinlupa City",
+        "motto" => "Strive for excellence in everything you do.",
+        "phone" => "+639530440570",
+        "facebook" => "https://www.facebook.com/bryan.bonula",
+        "email" => "bryancamayang01@gmail.com",
+    ],
+    [
+        "id" => "renzo-sheen",
+        "name" => "Renzo Sheen U. Malillin",
+        "image" => "images/renzo.jpg",
+        "age" => "22 Years Old",
+        "birthdate" => "May 22, 2004",
+        "address" => "Paranaque City",
+        "motto" => "Di man magwagi ang mahalaga nakibahagi",
+        "phone" => "+639070807240",
+        "facebook" => "https://www.facebook.com/re.nzo.56884",
+        "email" => "malillinrenzosheen_bsit@plmun.edu.ph",
+    ],
+    [
+        "id" => "jerson-santos",
+        "name" => "Jerson Santos Turcolas",
+        "image" => "images/jerson.jpg",
+        "age" => "20 Years Old",
+        "birthdate" => "June 06, 2006",
+        "address" => "San Pedro Laguna",
+        "motto" => "Time is GOLD",
+        "phone" => "+63605771882",
+        "facebook" => "https://www.facebook.com/Jerson03Santos#",
+        "email" => "turcolasjerson_bsit@plmun.edu.ph",
+    ],
+    [
+        "id" => "kingfroiland-paor",
+        "name" => "Kingfroiland Paor",
+        "image" => "images/king.png",
+        "age" => "20 Years Old",
+        "birthdate" => "November 24, 2005",
+        "address" => "Muntinlupa City",
+        "motto" => "Strive for excellence in everything you do.",
+        "phone" => "+639500447923",
+        "facebook" => "https://www.facebook.com/kingfroiland.paor.9#",
+        "email" => "paorkingfroiland_bsit@plmun.edu.ph",
+    ],
+    [
+        "id" => "mary-estoque",
+        "name" => "Mary Estoque",
+        "image" => "images/mary.jpg",
+        "age" => "19 Years Old",
+        "birthdate" => "August 27, 2006",
+        "address" => "Muntinlupa City",
+        "motto" => "everything happens for a reason.",
+        "phone" => "+639938210763",
+        "facebook" => "https://www.facebook.com/maryjoy.estoque.161#",
+        "email" => "maryjoyestoque06@gmail.com",
+    ],
+    [
+        "id" => "princess-famor",
+        "name" => "Princess Ann Famor",
+        "image" => "images/princess.jpg",
+        "age" => "20 Years Old",
+        "birthdate" => "October 24, 2005",
+        "address" => "Muntinlupa City",
+        "motto" => "“Your future is built by what you do today”.",
+        "phone" => "+639926319599",
+        "facebook" => "https://www.facebook.com/princessann.famor.1#",
+        "email" => "famorprincessann_bsit@plmun.edu.ph",
+    ],
+    [
+        "id" => "johndel",
+        "name" => "Johndel",
+        "image" => "images/johndel.jpg",
+        "age" => "21 Years Old",
+        "birthdate" => "August 02, 2005",
+        "address" => "19 Hyacinth St., South Green Heights Village, Putatan",
+        "motto" => "Walk your own path, even when no footprints exist.",
+        "phone" => "+639502839123",
+        "facebook" => "https://www.facebook.com/profile.php?id=61584894823871#",
+        "email" => "dorigjohndel_bsit@plmun.edu.ph",
+    ],
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,11 +104,12 @@
 
     </style>
 </head>
+
 <body>
 
     <!-- Header Section -->
     <header class="header">
-        <div class="header-title">GROUP-<span>4</span></div>
+        <div class="header-title"><?php echo $groupName; ?></div>
         <div class="header-search">
             <form id="searchForm" role="search" aria-label="Search team members">
                 <input id="searchInput" name="q" type="search" placeholder="Search team members..." aria-label="Search team members" />
@@ -37,212 +131,86 @@
 
         <!-- Team Grid Overview (reverse-triangle rows: 4,3,2,1) -->
         <section class="team-grid">
+
             <div class="team-row row-4">
-                <div class="grid-item">
-                    <div class="profile-img-wrapper">
-                        <img src="images/bryan.jpg" alt="Bryan Camayang">
+                <?php foreach (array_slice($teamMembers, 0, 4) as $member): ?>
+                    <div class="grid-item">
+                        <div class="profile-img-wrapper">
+                            <img src="<?php echo $member["image"]; ?>" alt="<?php echo $member["name"]; ?>">
+                        </div>
+
+                        <a href="#<?php echo $member["id"]; ?>" class="read-more">
+                            Read More
+                        </a>
                     </div>
-                    <a href="#bryan-camayang" class="read-more">Read More</a>
-                </div>
-                <div class="grid-item">
-                    <div class="profile-img-wrapper">
-                        <img src="images/renzo.jpg" alt="Renzo Sheen Malillin">
-                    </div>
-                    <a href="#renzo-sheen" class="read-more">Read More</a>
-                </div>
-                <div class="grid-item">
-                    <div class="profile-img-wrapper">
-                        <img src="images/jerson.jpg" alt="Jerson Santos Turcolas">
-                    </div>
-                    <a href="#jerson-santos" class="read-more">Read More</a>
-                </div>
-                <div class="grid-item">
-                    <div class="profile-img-wrapper">
-                        <img src="images/king.png" alt="Kingfroiland Paor">
-                    </div>
-                    <a href="#kingfroiland-paor" class="read-more">Read More</a>
-                </div>
+                <?php endforeach; ?>
             </div>
 
             <div class="team-row row-3">
-                <div class="grid-item">
-                    <div class="profile-img-wrapper">
-                        <img src="images/mary.jpg" alt="Mary Estoque">
+                <?php foreach (array_slice($teamMembers, 0, 3) as $member): ?>
+                    <div class="grid-item">
+                        <div class="profile-img-wrapper">
+                            <img src="<?php echo $member["image"]; ?>" alt="<?php echo $member['name']; ?>">
+                            <a href="#<?php echo $member["id"]; ?>" class="read-more"> Read More</a>
+                        </div>
                     </div>
-                    <a href="#mary-estoque" class="read-more">Read More</a>
-                </div>
-                <div class="grid-item">
-                    <div class="profile-img-wrapper">
-                        <img src="images/princess.jpg" alt="Princess Famor">
-                    </div>
-                    <a href="#princess-famor" class="read-more">Read More</a>
-                </div>
-                <div class="grid-item">
-                    <div class="profile-img-wrapper">
-                        <img src="images/johndel.jpg" alt="Johndel">
-                    </div>
-                    <a href="#johndel" class="read-more">Read More</a>
-                </div>
+                <?php endforeach ?>
             </div>
         </section>
 
         <!-- Detailed Profiles -->
         <section class="profile-list">
-            
-            <!-- Detail Card 1: Bryan -->
-            <div class="detail-card" id="bryan-camayang">
-                <div class="profile-img-wrapper" style="width: 140px; height: 140px; margin: 0;">
-                    <img src="images/bryan.jpg" alt="Bryan Camayang">
-                </div>
-                <div class="detail-info">
-                    <h2 class="detail-name">Bryan Camayang</h2>
-                    <p class="detail-bio">Age: 21 Years Old<br>
-                        <br>Birthdate: August 02, 2005
-                        <br>Location: Muntinlupa City
-                        <br>Motto: "Strive for excellence in everything you do."
+            <?php foreach ($teamMembers as $member): ?>
+                <div class="detail-card" id="<?php echo $member["id"]; ?>">
+                    <div class="profile-img-wrapper" style="width: 140px; height: 140px; margin: 0;">
+                        <img src="<?php echo $member["image"]; ?>" alt="<?php echo $member["name"]; ?>">
+                    </div>
+                    <h2 class="detail-name">
+                        <?php echo $member["name"]; ?>
+                    </h2>
+                    <p class="detail-bio">
+                        Age: <?php echo $member["age"]; ?><br><br>
+                        Birthdate: <?php echo $member["birthdate"]; ?> <br>
+                        Address: <?php echo $member["address"]; ?><br>
+                        Motto: "<?php echo $member["motto"]; ?>"
                     </p>
                     <ul class="contact-list">
-                        <li><i class="fa-solid fa-phone"></i> +639530440570</li>
-                        <li><i class="fa-solid fa-globe"></i> https://www.facebook.com/bryan.bonula</li>
-                        <li><i class="fa-regular fa-envelope"></i> bryancamayang01@gmail.com</li>
+                        <li>
+                            <i class="fa-solid fa-phone"></i>
+                            <?php echo $member["phone"]; ?>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-globe"></i>
+                            <a href="<?php echo $member["facebook"]; ?>" target="_blank">
+                                Facebook
+                            </a>
+                        </li>
+                        <li>
+                            <i class="fa-regular fa-envelope"></i>
+                            <a href="mailto:<?php echo $member["email"]; ?>">
+                                <?php echo $member["email"]; ?>
+                            </a>
+                        </li>
                     </ul>
                 </div>
-            </div>
 
-            <!-- Detail Card 2: Renzo -->
-            <div class="detail-card" id="renzo-sheen">
-                <div class="profile-img-wrapper" style="width: 140px; height: 140px; margin: 0;">
-                    <img src="images/renzo.jpg" alt="Renzo Sheen Malillin">
-                </div>
-                <div class="detail-info">
-                    <h2 class="detail-name">Renzo Sheen U. Malillin</h2>
-                    <p class="detail-bio">Age: 22 Years Old<br>
-                        <br>Birthdate: May 22, 2004
-                        <br>Location: Paranaque City
-                        <br>Motto: "Di man magwagi ang mahalaga nakibahagi"
-                    </p>
-                    <ul class="contact-list">
-                        <li><i class="fa-solid fa-phone"></i> +639070807240</li>
-                        <li><i class="fa-solid fa-globe"></i> https://www.facebook.com/re.nzo.56884</li>
-                        <li><i class="fa-regular fa-envelope"></i> malillinrenzosheen_bsit@plmun.edu.ph</li>
-                    </ul>
-                </div>
-            </div>
-
-            <!-- Detail Card 3: Jerson -->
-            <div class="detail-card" id="jerson-santos">
-                <div class="profile-img-wrapper" style="width: 140px; height: 140px; margin: 0;">
-                    <img src="images/jerson.jpg" alt="Jerson Santos Turcolas">
-                </div>
-                <div class="detail-info">
-                    <h2 class="detail-name">Jerson Santos Turcolas</h2>
-                    <p class="detail-bio">Age: 20 Years Old<br>
-                        <br>Birthdate: June 06, 2006
-                        <br>Address: San Pedro Laguna
-                        <br>Motto: "Time is GOLD"
-                    </p>
-                    <ul class="contact-list">
-                        <li><i class="fa-solid fa-phone"></i>+63605771882</li>
-                        <li><i class="fa-solid fa-globe"></i> https://www.facebook.com/Jerson03Santos#
-                        <li><i class="fa-regular fa-envelope"></i>turcolasjerson_bsit@plmun.edu.ph</li>
-                    </ul>
-                </div>
-            </div>
-
-            <!-- Detail Card 4: Kingfroiland -->
-            <div class="detail-card" id="kingfroiland-paor">
-                <div class="profile-img-wrapper" style="width: 140px; height: 140px; margin: 0;">
-                    <img src="images/king.png" alt="Kingfroiland Paor">
-                </div>
-                <div class="detail-info">
-                    <h2 class="detail-name">Kingfroiland Paor </h2>
-                    <p class="detail-bio">Age: 20 Years Old<br>
-                        <br>Birthdate: November 24, 2005
-                        <br>Address: Muntinlupa City
-                        <br>Motto: "Strive for excellence in everything you do."
-                    </p>
-                    <ul class="contact-list">
-                        <li><i class="fa-solid fa-phone"></i> +639500447923</li>
-                        <li><i class="fa-solid fa-globe"></i> https://www.facebook.com/kingfroiland.paor.9#</li>
-                        <li><i class="fa-regular fa-envelope"></i> paorkingfroiland_bsit@plmun.edu.ph</li>
-                    </ul>
-                </div>
-            </div>
-
-            <!-- Detail Card 5: Mary -->
-            <div class="detail-card" id="mary-estoque">
-                <div class="profile-img-wrapper" style="width: 140px; height: 140px; margin: 0;">
-                    <img src="images/mary.jpg" alt="Mary Estoque">
-                </div>
-                <div class="detail-info">
-                    <h2 class="detail-name">Mary Estoque </h2>
-                    <p class="detail-bio">Age: 19 Years Old<br>
-                        <br>Birthdate: August 27, 2006
-                        <br>Address: Muntinlupa City
-                        <br>Motto: "everything happens for a reason."
-                    </p>
-                    <ul class="contact-list">
-                        <li><i class="fa-solid fa-phone"></i>+639938210763</li>
-                        <li><i class="fa-solid fa-globe"></i> https://www.facebook.com/maryjoy.estoque.161#</li>
-                        <li><i class="fa-regular fa-envelope"></i>maryjoyestoque06@gmail.com</li>
-                    </ul>
-                </div>
-            </div>
-
-            <!-- Detail Card 6: Princess -->
-            <div class="detail-card" id="princess-famor">
-                <div class="profile-img-wrapper" style="width: 140px; height: 140px; margin: 0;">
-                    <img src="images/princess.jpg" alt="Princess Famor">
-                </div>
-                <div class="detail-info">
-                    <h2 class="detail-name">Princess Ann Famor</h2>
-                    <p class="detail-bio">Age: 20 Years Old<br>
-                        <br>Birthdate: October 24, 2005
-                        <br>Location: Muntinlupa City
-                        <br>Motto: “Your future is built by what you do today”.
-                    </p>
-                    <ul class="contact-list">
-                        <li><i class="fa-solid fa-phone"></i> +639926319599</li>
-                        <li><i class="fa-solid fa-globe"></i> https://www.facebook.com/princessann.famor.1#</li>
-                        <li><i class="fa-regular fa-envelope"></i> famorprincessann_bsit@plmun.edu.ph</li>
-                    </ul>
-                </div>
-            </div>
-
-            <!-- Detail Card 7: Johndel -->
-            <div class="detail-card" id="johndel">
-                <div class="profile-img-wrapper" style="width: 140px; height: 140px; margin: 0;">
-                    <img src="images/johndel.jpg" alt="Johndel">
-                </div>
-                <div class="detail-info">
-                    <h2 class="detail-name">Johndel </h2>
-                    <p class="detail-bio">Age: 21 Years Old<br>
-                        <br>Birthdate: August 02, 2005
-                        <br>Address: 19 Hyacinth St., South Green Heights Village, Putatan
-                        <br>Motto: "Walk your own path, even when no footprints exist."
-                    </p>
-                    <ul class="contact-list">
-                        <li><i class="fa-solid fa-phone"></i>+639502839123</li>
-                        <li><i class="fa-solid fa-globe"></i> https://www.facebook.com/profile.php?id=61584894823871#</li>
-                        <li><i class="fa-regular fa-envelope"></i>dorigjohndel_bsit@plmun.edu.ph</li>
-                    </ul>
-                </div>
-            </div>
-
+            <?php endforeach; ?>
         </section>
 
         <!-- Footer -->
-        
+
     </div>
     <script src="system.js"></script>
-      <footer class="site-footer">
-            <div class="container footer-inner">
-                <div class="footer-center">
-                    <div class="footer-brand">GROUP-<span>4</span></div>
-                    <p class="footer-text">IT3J-INTPROG</p>
-                    <p class="copyright">© <span id="year"></span> GROUP-4. All rights reserved.</p>
-                </div>
+    <footer class="site-footer">
+        <div class="container footer-inner">
+            <div class="footer-center">
+                <div class="footer-brand"><?php echo $groupName; ?></div>
+                <p class="footer-text">IT3J-INTPROG</p>
+                <p class="copyright">
+                    © <?php echo date ("Y");?> <?php echo $groupName;?>. All rights reserved.</p>
             </div>
-        </footer>
+        </div>
+    </footer>
 </body>
+
 </html>
